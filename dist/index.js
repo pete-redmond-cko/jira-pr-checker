1fe535bad4f1018181b936dd1f9997b74488b741
Peter.Redmond <peter.redmond@checkout.com>
2020-04-17 09:21:23 +0100

    whitelist branches

--- dist/index.js
+++ dist/index.js
@@ -2028,21 +2028,34 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(__webpack_require__(470));
 const github = __importStar(__webpack_require__(469));
+const ignoreBranchTerms = ['dependabot'];
+const ignoreBranch = (branch, ignoreBranchTerms) => {
+    for (let i = 0; i < ignoreBranchTerms.length; i++) {
+        const branchTerm = ignoreBranchTerms[i];
+        if (branch.startsWith(branchTerm)) {
+            return true;
+        }
+    }
+    return false;
+};
 function run() {
-    var _a, _b;
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const github_token = core.getInput('GITHUB_TOKEN');
+            const github_token = core.getInput('GITHUB_TOKEN', { required: true });
             const octokit = new github.GitHub(github_token);
+            const ignoreBranchTerms = core.getInput('branch-term-whitelist').split(',');
             const context = github.context;
-            if (context.payload.pull_request == null) {
+            const pullRequest = context.payload.pull_request;
+            if (pullRequest == null) {
                 core.setFailed('No pull request found.');
                 return;
             }
-            const pull_request_number = context.payload.pull_request.number;
-            const title = (_b = (_a = context.payload) === null || _a === void 0 ? void 0 : _a.pull_request) === null || _b === void 0 ? void 0 : _b.title;
-            if (!/^((?<!([A-Z])-?)[A-Z]+-\d+)/.test(title)) {
-                const body = `PR title must start with a valid JIRA ticket ie COVID-19`;
+            const pull_request_number = pullRequest.number;
+            const title = pullRequest.title;
+            const branch = context.ref;
+            if (!/^((?<!([A-Z])-?)[A-Z]+-\d+)/.test(title) &&
+                !ignoreBranch(branch, ignoreBranchTerms)) {
+                const body = `PR title must start with a valid JIRA ticket number (COVID-19)`;
                 yield octokit.issues.createComment(Object.assign(Object.assign({}, context.repo), { issue_number: pull_request_number, body }));
             }
         }
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,44 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
+const ignoreBranchTerms = ['dependabot'];
+
+const ignoreBranch = (branch: string, ignoreBranchTerms: string[]) => {
+  for (let i = 0; i < ignoreBranchTerms.length; i++) {
+    const branchTerm = ignoreBranchTerms[i];
+
+    if (branch.startsWith(branchTerm)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 async function run(): Promise<void> {
   try {
-    const github_token = core.getInput('GITHUB_TOKEN');
+    const github_token = core.getInput('GITHUB_TOKEN', {required: true});
     const octokit = new github.GitHub(github_token);
+    const ignoreBranchTerms = core.getInput('branch-term-whitelist').split(',');
 
     const context = github.context;
 
-    if (context.payload.pull_request == null) {
+    const pullRequest = context.payload.pull_request;
+
+    if (pullRequest == null) {
       core.setFailed('No pull request found.');
       return;
     }
 
-    const pull_request_number = context.payload.pull_request.number;
-    const title = context.payload?.pull_request?.title;
+    const pull_request_number = pullRequest.number;
+    const title = pullRequest.title;
+    const branch = context.ref;
 
-    if (!/^((?<!([A-Z])-?)[A-Z]+-\d+)/.test(title)) {
-      const body = `PR title must start with a valid JIRA ticket ie COVID-19`;
+    if (
+      !/^((?<!([A-Z])-?)[A-Z]+-\d+)/.test(title) &&
+      !ignoreBranch(branch, ignoreBranchTerms)
+    ) {
+      const body = `PR title must start with a valid JIRA ticket number (COVID-19)`;
 
       await octokit.issues.createComment({
         ...context.repo,
